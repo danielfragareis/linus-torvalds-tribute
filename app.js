38d288aa89ee26d4d4368223eae98763a33e2b64
danielfragareis <danielfreis91@gmail.com>
2022-12-30 18:36:36 -0300

    atualização app.js
    atualização das cores do dark mode on

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
  document.querySelector("button#theme");
- let itsDark = false;
+ let itsDark = false, 
+ header = document.getElementsByTagName("header"), 
+ style = document.documentElement.style,
+ menu = document.getElementsByClassName("menu");
 
  document.getElementById("theme").addEventListener('click', () => {
     itsDark = (itsDark) ? false : true;
@@ -11,11 +14,15 @@ function change_theme() {
 };
 
 const darkModeOn = () => {
-    document.documentElement.style.setProperty("background-color", "black");
-    document.documentElement.style.setProperty("color", "white");
+    style.setProperty("background-color", "black");
+    style.setProperty("color", "white");
+    header[0].style.backgroundImage = "linear-gradient(to bottom, rgb(166, 166, 165) , rgb(117, 117, 117), rgb(3, 3, 2))";
+    menu[0].style.backgroundColor = "rgb(166, 166, 165)";
+    menu[0].style.border = ("1px solid rgba(250, 250, 250, 0.5)");
 };
 
 const darkModeOff = () => {
     document.documentElement.style.setProperty("background-color", "white");
     document.documentElement.style.setProperty("color", "black");
+    header[0].style.backgroundImage = "linear-gradient(to bottom, rgb(200, 200, 200), red, transparent)";
 };
